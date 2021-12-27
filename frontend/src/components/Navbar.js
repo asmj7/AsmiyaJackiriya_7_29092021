954a8fdc98749d6921480b99e6397ad378190c53
Asmiya Jackiriya <asmiyaj77@gmail.com>
2021-12-27 19:41:26 +0100

    Adding tabindex

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -152,7 +152,7 @@ export default function Navbar() {
         <div className="navbar">
 
             <Link to="/" className="link">
-                <img className="groupomania" src="assets/icon-left-font-monochrome-white"/>
+                <img tabindex="0" className="groupomania" src="assets/icon-left-font-monochrome-white"/>
             </Link>
             <ul className="menu">
                 {loggedIn ? (
